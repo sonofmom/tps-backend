a073a90250ca02ce6c4658a46163a1929f2cd70a
K-Dimentional Tree <kdimentionaltree@gmail.com>
2023-10-16 16:56:07 +0300

    Fixed bug

--- backend/api/app.py
+++ backend/api/app.py
@@ -20,6 +20,7 @@ app = FastAPI(docs_url='/')
 def get_tps(from_datetime: datetime=Query(None, title='From date'),
             to_datetime: datetime=Query(None, title='To date'),
             limit: int=Query(2048, title='Max records in response', ge=0, le=2048),
+            seqno_continuation: int=Query(None, title='Set seqno for batch reading'),
             drop_zeros: bool=Query(False, title='Drop records with tx_delta==0'),
             sort: str=Query('desc', title='Sort order', enum=['asc', 'desc']),
             db: Client=Depends(db_dep)):
@@ -30,11 +31,13 @@ def get_tps(from_datetime: datetime=Query(None, title='From date'),
         where_clause.append(f"timestamp >= {from_datetime}")
     if to_datetime is not None:
         where_clause.append(f"timestamp <= {to_datetime}")
+    if seqno_continuation is not None:
+        where_clause.append(f"seqno < {seqno_continuation}")
+    if drop_zeros:
+        where_clause.append(f' where tx_count_delta > 0')
     if where_clause:
         where_clause = ' and '.join(*where_clause)
-        query += ' ' + where_clause
-    if drop_zeros:
-        query += ' where tx_count_delta > 0'
+        query += ' where ' + where_clause
     query += f' order by seqno {sort}'
     query += f' limit {limit}'
     logger.info(f'Query: {query}')
